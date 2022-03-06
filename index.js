765458090b7c3f243f80a0f5aa5e3858f9a4e351
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-03-06 18:32:10 -0500

    Use httpOnly cookies to authenticate all backend routes, implement ContextApi for handling logged state in client

--- index.js
+++ index.js
@@ -3,15 +3,86 @@ const cors = require("cors");
 const { MongoClient } = require('mongodb')
 const { v4: uuidv4 } = require('uuid')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser')
 require("dotenv").config();
 const uri = "mongodb+srv://user:Password@cluster0.b4wso.mongodb.net/Cluster0?retryWrites=true&w=majority"
-
 const app = express();
-app.use(cors());
+app.use(morgan('dev'))
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser())
 
 const PORT = process.env.PORT || 3001;
+
+
+const authenticateToken = async (req, res, next) => {
+
+  try {
+
+    console.log(req.headers.cookie, 'req.headers.cookie')
+
+    const token = req.headers.cookie.split('=')[1];
+
+
+    //Return if they have no token
+    if (!token) return res.status(401).send('Token required for access! Please login.')
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, userId) => {
+    //They have a token, but it has expired so they no longer have access
+    if (err) return res.status(403).send("Unauthorized Access token!")
+
+    //Set user on request object
+    req.userId = userId;
+
+    //Token is authenticated, proceed with route
+    next()
+  })
+
+  } catch(err) {
+    // console.log(err, 'authenticate')
+    res.status(500)
+    .clearCookie('watchlist-jwt')
+    .send("Invalid token received! Please Login again")
+    
+
+    
+  }
+
+  
+}
+
+
+
+
+const returnToken = (userId, statusCode, res) => {
+  const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+    expiresIn: '1h'
+  })
+  const options = {
+    httpOnly: true, 
+    expires: new Date(new Date().getTime() + (60 * 60 * 1000)), 
+    sameSite: 'strict',
+    path: "/"
+  }
+
+
+  console.log('jwt token for:', userId)
+  // console.log(process.env.JWT_EXPIRY)
+  // console.log(Number(process.env.JWT_EXPIRY))
+
+  // userId: user.user_id,
+  res.status(statusCode)
+  .cookie('watchlist-jwt', token, options)
+  .send('cookie initialized')
+}
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.json('hello')
@@ -58,12 +129,11 @@ app.post('/signup', async (req, res) => {
     const insertedUser = await users.insertOne(data)
     console.log('inserted', insertedUser)
 
-    const token = jwt.sign({user: generatedUserId}, process.env.JWT_SECRET, {
-      expiresIn: '1h'
-    })
+
+    returnToken(generatedUserId, 200, res);
 
     // userId: generatedUserId,
-    res.status(201).json({ token, username: req.body.username, sanitizedEmail })
+
 
   } catch(err) {
     console.log(err)
@@ -90,20 +160,15 @@ app.post('/login', async(req, res) => {
 
     const passwordMatch = bcrypt.compare(password, user.hashed_password)
 
+  
     //If credentials are invalid, let user know
     if(!user || !passwordMatch) {
       return res.status(400).send('Invalid Credentials');
     }
 
     //Else, generate a token and return data to client
-    const token = jwt.sign({user: user.user_id}, process.env.JWT_SECRET, {
-      expiresIn: '1h'
-    })
-    console.log('USERNAME', username)
-    console.log(user)
 
-    // userId: user.user_id,
-    res.status(201).json({ token, username, email })
+    returnToken(user.user_id, 200, res);
 
 
   } catch(err) {
@@ -114,11 +179,21 @@ app.post('/login', async(req, res) => {
 
 })
 
-app.put('/my-list', async (req, res) => {
+app.get('/logout', async(req, res) => {
+
+      res.status(201)
+      .clearCookie('watchlist-jwt')
+      .send('cookie deleted')
+
+})
+
+app.put('/my-list', authenticateToken, async (req, res) => {
   const client = new MongoClient(uri);
   console.log('my list')
 
-  const { title, id, category, email, poster_path, backdrop_path, rating, year } = req.body
+  const userId = req.userId.userId;
+
+  const { title, id, category, poster_path, backdrop_path, rating, year } = req.body
   const newWatchlistItem = {
     title,
     tmdb_id: id,
@@ -138,7 +213,7 @@ app.put('/my-list', async (req, res) => {
     await client.connect()
     const database = client.db('app-data')
     const users = database.collection('users')
-    const user = await users.findOne({email})
+    const user = await users.findOne({user_id: userId})
     console.log('USER', user)
     const foundItem = user.watchlist.find(item => item.tmdb_id === id)
 
@@ -147,7 +222,7 @@ app.put('/my-list', async (req, res) => {
       throw new Error("item already exists!")
     }
 
-    const filter = { email };
+    const filter = { user_id: userId };
 
     const updateDoc = {
       $addToSet: {
@@ -175,11 +250,12 @@ app.put('/my-list', async (req, res) => {
 
 
 // Get my watchlist
-app.post('/my-list', async (req, res) => {
+app.get('/my-list', authenticateToken, async (req, res) => {
   const client = new MongoClient(uri);
-  console.log(req.body)
+ 
+  console.log(req.userId, 'user id')
 
-  const { email } = req.body
+  const userId = req.userId.userId;
 
 
   try {
@@ -189,7 +265,7 @@ app.post('/my-list', async (req, res) => {
     const users = database.collection('users')
 
     //Attempt to retrieve user info from db by email
-    const user = await users.findOne({ email });
+    const user = await users.findOne({ user_id: userId });
     console.log(user)
 
 
@@ -198,17 +274,19 @@ app.post('/my-list', async (req, res) => {
 
   } catch(err) {
     console.log(err)
+    
   } finally {
     await client.close()
   }
 })
 
-app.put('/user-rating', async (req, res) => {
+app.put('/user-rating', authenticateToken, async (req, res) => {
   const client = new MongoClient(uri);
 
-  console.log(req.body)
-
-  const { email, frontendRating, mediaId } = req.body
+  const userId = req.userId.userId;
+  console.log(userId)
+  console.log(req.userId, 'id')
+  const { frontendRating, mediaId } = req.body
 
 
   try {
@@ -218,7 +296,7 @@ app.put('/user-rating', async (req, res) => {
     const users = database.collection('users')
 
     //Attempt to retrieve user info from db by email
-    const user = await users.findOne({ email });
+    const user = await users.findOne({ user_id: userId });
     console.log(user)
 
     const userWatchlist = user.watchlist;
@@ -235,7 +313,7 @@ app.put('/user-rating', async (req, res) => {
     const updatedWatchlist = [...userWatchlist]
     updatedWatchlist[selectedWatchlistItemIndex] = updatedWatchlistItem
 
-    const filter = { email };
+    const filter = { user_id: userId };
 
     const updateDoc = {
       $set: {
@@ -265,12 +343,13 @@ app.put('/user-rating', async (req, res) => {
 
 
 
-app.put('/user-watched', async (req, res) => {
+app.put('/user-watched', authenticateToken, async (req, res) => {
   const client = new MongoClient(uri);
 
   console.log(req.body)
 
-  const { email, frontendWatched, mediaId } = req.body
+  const userId = req.userId.userId;
+  const { frontendWatched, mediaId } = req.body
 
 
   try {
@@ -280,7 +359,7 @@ app.put('/user-watched', async (req, res) => {
     const users = database.collection('users')
 
     //Attempt to retrieve user info from db by email
-    const user = await users.findOne({ email });
+    const user = await users.findOne({ user_id: userId });
     console.log(user)
 
     const userWatchlist = user.watchlist;
@@ -297,7 +376,7 @@ app.put('/user-watched', async (req, res) => {
     const updatedWatchlist = [...userWatchlist]
     updatedWatchlist[selectedWatchlistItemIndex] = updatedWatchlistItem
 
-    const filter = { email };
+    const filter = { user_id: userId };
 
     const updateDoc = {
       $set: {
@@ -328,12 +407,13 @@ app.put('/user-watched', async (req, res) => {
 
 
 
-app.put('/user-liked', async (req, res) => {
+app.put('/user-liked', authenticateToken, async (req, res) => {
   const client = new MongoClient(uri);
 
   console.log(req.body)
 
-  const { email, frontendLike, mediaId } = req.body
+  const userId = req.userId.userId;
+  const { frontendLike, mediaId } = req.body
 
 
   try {
@@ -343,7 +423,7 @@ app.put('/user-liked', async (req, res) => {
     const users = database.collection('users')
 
     //Attempt to retrieve user info from db by email
-    const user = await users.findOne({ email });
+    const user = await users.findOne({ user_id: userId });
     console.log(user)
 
     const userWatchlist = user.watchlist;
@@ -360,7 +440,7 @@ app.put('/user-liked', async (req, res) => {
     const updatedWatchlist = [...userWatchlist]
     updatedWatchlist[selectedWatchlistItemIndex] = updatedWatchlistItem
 
-    const filter = { email };
+    const filter = { user_id: userId };
 
     const updateDoc = {
       $set: {
@@ -390,12 +470,13 @@ app.put('/user-liked', async (req, res) => {
 
 
 
-app.put('/delete-item', async (req, res) => {
+app.put('/delete-item', authenticateToken, async (req, res) => {
   const client = new MongoClient(uri);
 
   console.log('body', req.body)
 
-  const { email, mediaId } = req.body
+  const userId = req.userId.userId;
+  const { mediaId } = req.body
 
 
   try {
@@ -405,7 +486,7 @@ app.put('/delete-item', async (req, res) => {
     const users = database.collection('users')
 
     //Attempt to retrieve user info from db by email
-    const user = await users.findOne({ email });
+    const user = await users.findOne({ user_id: userId });
     console.log(user)
 
     const userWatchlist = user.watchlist;
@@ -417,7 +498,7 @@ app.put('/delete-item', async (req, res) => {
       return item.tmdb_id !== mediaId
     })
 
-    const filter = { email };
+    const filter = { user_id: userId };
 
     const updateDoc = {
       $set: {
@@ -442,6 +523,38 @@ app.put('/delete-item', async (req, res) => {
     await client.close()
   }
 
+})
+
+app.get('/logged', authenticateToken, async (req, res) => {
+//   const client = new MongoClient(uri);
+
+
+  const userId = req.userId.userId;
+  console.log(userId)
+  console.log(req.userId, 'user id first')
+
+
+
+
+//   try {
+//     //Connect to MongoDB database
+//     await client.connect()
+//     const database = client.db('app-data')
+//     const users = database.collection('users')
+
+//     //Attempt to retrieve user info from db by email
+//     const user = await users.findOne({ user_id: userId });
+//     console.log(user)
+
+
+    return res.status(201).send("Auth passed, user logged in!")
+
+
+  // } catch(err) {
+  //   console.log(err)
+  // } finally {
+  //   await client.close()
+  // }
 })
 
 
