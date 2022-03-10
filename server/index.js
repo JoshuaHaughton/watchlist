f20e1ff455f0b1d0f06cd04dfe33a340918593e9
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-03-09 22:39:23 -0500

    Adjust server logic for netlify - heroku connection

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const uri =
 const app = express();
 app.use(morgan("dev"));
 //Allows for server to receive requests from specific domains
-app.use(cors({ credentials: true, origin: "https://watchlist-client.netlify.app/" }));
+app.use(cors({ credentials: true, origin: "https://watchlist-client.netlify.app" }));
 app.use(express.json());
 app.use(cookieParser());
 
